d779c590e66d256023541ae9d8e33b627bdd4088
Annie Wiegel <annewiegel17@gmail.com>
2021-02-04 01:10:36 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+#attendees #room_assignments #
+def badge_maker(name)
+ return "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(attendees)
+  badges = []
+  attendees.each do |name|
+   badge = "Hello, my name is #{name}."
+  badges << badge
+end
+return badges
+end
+
+
+def assign_rooms
+
+end
+
+
+def printer
+
+end
